ac49fba88eeb3e77657ce25a27689f2c93acfc7f
aleksaar0n <67913136+aleksaar0n@users.noreply.github.com>
2025-08-30 12:00:01 +0200

    Update register-logic.php

--- register-logic.php
+++ register-logic.php
@@ -1,5 +1,6 @@
 <?php
 
+    ### PREBACEN KOD U register.php zbog povratnih informacija o greskama ###
     ### TODO ###
     # Provera da li input-i iz forme postoje i da li su prazni
     # Provera da li je "first_name" vece od jednog karaktera
@@ -7,6 +8,7 @@
     # Provera ispravnosti mejla 
     # Provera da li se ponovljeni unos password-a poklapa sa primarnim
     # Provera formata broja telefona preko REGEX-a
+    
 
     require_once("database-connection.php");
     require_once("functions.php");
